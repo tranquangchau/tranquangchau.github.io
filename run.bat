7b7e8b9a299858d28ea39bf5714667a698903873
tranquangchau <tranquangchau155@gmail.com>
2017-05-13 21:44:32 +0700

    have add comment for push

--- run.bat
+++ run.bat
@@ -2,6 +2,8 @@
 git pull
 git status
 git add --all
-git commit -a -m "auto"
+SET /P _inputname= Please enter an comment commit: 
+IF "%_inputname%"=="OMG" "%_inputname%"="auto" 
+git commit -a -m "%_inputname%"
 git push
 pause
